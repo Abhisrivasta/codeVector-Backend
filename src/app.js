dde5540869a144e1f235b826b3fa55f750753e08
Abhishek Srivastava <as6338830@gmail.com>
2026-06-23 20:43:33 +0530

    fix cors issue

--- src/app.js
+++ src/app.js
@@ -1,10 +1,28 @@
 import express from "express";
+import cors from "cors";
 import productRoutes from "./routes/product.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://code-vector-frontend-blond.vercel.app",
+    ],
+    credentials: true,
+  })
+);
+
 app.use("/products", productRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Server is running 🚀",
+  });
+});
 
 export default app;
--- src/server.js
+++ src/server.js
@@ -1,18 +1,7 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-import cors from "cors";
-
 
 dotenv.config();
-
-app.use(
-  cors({
-    origin: [
-      "http://localhost:5173",
-      "https://code-vector-frontend-blond.vercel.app",
-    ],
-  })
-);
 
 const PORT = process.env.PORT || 5000;
 
