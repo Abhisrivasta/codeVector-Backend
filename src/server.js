07c71578c47a3b9d17b3ed042e2d032bdf25055d
Abhishek Srivastava <as6338830@gmail.com>
2026-06-23 17:16:56 +0530

    added cors

--- src/server.js
+++ src/server.js
@@ -5,7 +5,15 @@ import cors from "cors";
 
 dotenv.config();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://code-vector-frontend-blond.vercel.app",
+    ],
+  })
+);
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
